526d8ba6c15245d069e6b116a6b4e1002b112bfe
mengcz13 <mengcz13@163.com>
2017-06-01 11:53:23 +0800

    add truncation

--- main.py
+++ main.py
@@ -8,20 +8,61 @@ from torch.autograd import Variable
 from torch import optim
 import numpy as np
 from argparse import ArgumentParser
+import shutil
+import itchat
+import datetime
+import sys
+
+itchat.auto_login(enableCmdQR=2, hotReload=True)
+
+original_stdout = sys.stdout
+
+
+class RedirectedWechatOut(object):
+    def write(self, s):
+        try:
+            itchat.send(('%s' % (s)).rstrip('\r\n'), toUserName='filehelper')
+            original_stdout.write(s)
+        except:
+            pass
+
+
+redirectedWechatOut = RedirectedWechatOut()
+sys.stdout = redirectedWechatOut
+
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('--datapath', type=str, default='./data',
                         help='path to the data folder, which contains GoogleNews-vectors and training data')
+    parser.add_argument('--word2vecmodelfile', type=str, default='GoogleNews-vectors-negative300.bin',
+                        help='file name of pre-trained word2vec model')
+    parser.add_argument('--train_ratio', type=float, default=0.7,
+                        help='ratio of train set in all data')
+    parser.add_argument('--remove_stopwords', action='store_true',
+                        help='flag for removing stopwords')
     parser.add_argument('--batch_size', type=int, default=100,
                         help='batch size in each iteration')
-    parser.add_argument('--iter', type=int, default=50,
-                        help='iteration times')
+    parser.add_argument('--epoch', type=int, default=50,
+                        help='epoch times')
     parser.add_argument('--cuda', action='store_true',
                         help='use if you want the model to run on CUDA')
+    parser.add_argument('--iterpbatch', type=int, default=1,
+                        help='iteration time of per batch')
+    parser.add_argument('--lr', type=float, default=1e-3,
+                        help='learning rate')
+    parser.add_argument('--weight_decay', type=float, default=1e-6,
+                        help='weight decay')
+    parser.add_argument('--title_trunc', type=int, default=20,
+                        help='truncate title')
+    parser.add_argument('--body_trunc', type=int, default=100,
+                        help='truncate body')
     options = parser.parse_args()
+    print(options)
 
-    traindata = Data(options.datapath)
+    traindata = Data(options.datapath, options.word2vecmodelfile, options.train_ratio, options.remove_stopwords,
+                     options.title_trunc,
+                     options.body_trunc)
 
     # Build model, optimizer and loss function
     nnmodel = NNModel(title_input_size=(1, traindata.maxtitlelen, traindata.vecdim),
@@ -34,26 +75,97 @@ def main():
     if options.cuda:
         nnmodel = nnmodel.cuda()
 
-    optimizer = optim.Adadelta(nnmodel.parameters(), lr=1e-4, weight_decay=1e-6)
+    optimizer = optim.Adadelta(nnmodel.parameters(), lr=options.lr, weight_decay=options.weight_decay)
     loss_func = nn.CrossEntropyLoss()
+    if options.cuda:
+        loss_func = loss_func.cuda()
 
     batch_size = options.batch_size
-    for t in range(options.iter):
-        # generate a batch
-        binput, boutput = traindata.get_batch(batch_size)
-        if options.cuda:
-            binput, boutput = binput.cuda(), boutput.cuda()
 
-        # do optimization
-        optimizer.zero_grad()
-        boutput_pred_score = nnmodel(binput)
-        loss = loss_func(boutput_pred_score, boutput)
-        _, boutput_pred = torch.max(boutput_pred_score, dim=1)
-        boutput_pred = boutput_pred.view(-1)
-        accuracy = torch.mean(torch.eq(boutput_pred, boutput).float())
-        print(t, 'loss =', loss.data[0], 'accu =', accuracy.data[0])
-        loss.backward()
-        optimizer.step()
+    best_accu = 0
+
+    save_checkpoint({
+        'epoch': -1,
+        'cpoch_num': -1,
+        'state_dict': nnmodel.state_dict(),
+        'best_accu': best_accu,
+        'optimizer': optimizer.state_dict()
+    }, True)
+
+    starttime = datetime.datetime.now()
+    print('Start training at: %s' % starttime)
+
+    for t in range(options.epoch):
+
+        cpoch_num = 0
+
+        while cpoch_num < traindata.trainsize:
+            cpoch_starttime = datetime.datetime.now()
+            # generate a batch
+            binput, boutput = traindata.train_get_batch(batch_size)
+            print('A Train Batch is generated!')
+            cpoch_num += boutput.size()[0]
+            if options.cuda:
+                binput, boutput = binput.cuda(), boutput.cuda()
+            binput = Variable(binput, requires_grad=True)
+            boutput = Variable(boutput, requires_grad=False)
+            for t2 in range(options.iterpbatch):
+                # do optimization
+                optimizer.zero_grad()
+                boutput_pred_score = nnmodel(binput)
+                loss = loss_func(boutput_pred_score, boutput)
+                currtime = datetime.datetime.now()
+                usedtime = currtime - cpoch_starttime
+                esttime = usedtime * (traindata.trainsize - cpoch_num) / (cpoch_num)
+                print('%s epoch=%s, cpoch_num=%s, iterpbatch=%s, loss=%s, usedtime=%s' % (
+                    currtime, t, cpoch_num, t2, loss.data[0], usedtime))
+                loss.backward()
+                optimizer.step()
+            _, boutput_pred = torch.max(boutput_pred_score, dim=1)
+            boutput_pred = boutput_pred.view(-1)
+            batch_accu = torch.mean(torch.eq(boutput_pred, boutput).float())
+            currtime = datetime.datetime.now()
+            usedtime = currtime - starttime
+            esttime = usedtime * (traindata.trainsize - cpoch_num) / (cpoch_num)
+            print('epoch=%s, cpoch_num=%s, batch_accu=%s, esttime=%s' % (t, cpoch_num, batch_accu.data[0], esttime))
+
+            if cpoch_num >= traindata.trainsize:
+                vpoch_num = 0
+                mean_accu = 0
+                while vpoch_num < traindata.validatesize:
+                    vinput, voutput = traindata.validate_get_batch(batch_size)
+                    vpoch_num += voutput.size()[0]
+                    if options.cuda:
+                        vinput, voutput = vinput.cuda(), voutput.cuda()
+                    vinput = Variable(vinput, requires_grad=True)
+                    voutput = Variable(voutput, requires_grad=False)
+                    voutput_pred_score = nnmodel(vinput)
+                    _, voutput_pred = torch.max(voutput_pred_score, dim=1)
+                    voutput_pred = voutput_pred.view(-1)
+                    accuracy = torch.mean(torch.eq(voutput_pred, voutput).float())
+                    mean_accu += (voutput.size()[0] * accuracy.data[0])
+                    print('vpoch_num=%s, temp_accuracy=%s' % (vpoch_num, accuracy.data[0]))
+                mean_accu = mean_accu / traindata.validatesize
+                print('epoch=%s, validation accuracy=%s' % (t, mean_accu))
+
+                isbest = (mean_accu > best_accu)
+                save_checkpoint({
+                    'epoch': t,
+                    'cpoch_num': cpoch_num,
+                    'state_dict': nnmodel.state_dict(),
+                    'best_accu': best_accu,
+                    'accu': mean_accu,
+                    'optimizer': optimizer.state_dict()
+                }, isbest, filename='checkpoint_%s.pth.tar' % t)
+                if isbest:
+                    best_accu = mean_accu
+
+
+def save_checkpoint(state, isbest, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if isbest:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
 
 if __name__ == '__main__':
     main()
--- word2vec_model.py
+++ word2vec_model.py
@@ -12,10 +12,11 @@ from torch.autograd import Variable
 
 
 class Data(object):
-    def __init__(self, datapath='./data'):
-        word2vec_bin_path = os.path.join(datapath, 'GoogleNews-vectors-negative300.bin')
+    def __init__(self, datapath, word2vecmodelfile, train_ratio, remove_stopwords, title_trunc, body_trunc):
+        word2vec_bin_path = os.path.join(datapath, word2vecmodelfile)
         train_bodies_path = os.path.join(datapath, 'train_bodies.csv')
         train_stances_path = os.path.join(datapath, 'train_stances.csv')
+        self.remove_stopwords = remove_stopwords
 
         with open(train_bodies_path, 'r', encoding='utf-8') as bodiesf:
             with open(train_stances_path, 'r', encoding='utf-8') as stancesf:
@@ -30,8 +31,9 @@ class Data(object):
                 print('Preparing article list...')
                 self.article_list = []
                 for stance in stances:
-                    self.article_list.append((self._preprocess_text(stance['Headline']),
-                                              self._preprocess_text(id2body_map[int(stance['Body ID'])]),
+                    self.article_list.append((self._preprocess_text(stance['Headline'], trunc=title_trunc),
+                                              self._preprocess_text(id2body_map[int(stance['Body ID'])],
+                                                                    trunc=body_trunc),
                                               stance['Stance']))
                 print(self.article_list[0])
 
@@ -42,49 +44,84 @@ class Data(object):
 
         print('Generating statistics...')
         self.maxtitlelen = max((len(a[0]) for a in self.article_list))
+        print('Max length of title:', self.maxtitlelen)
+        self.maxtitlelen = max((self.maxtitlelen, title_trunc))
+        print('Truncated title length:', self.maxtitlelen)
         self.maxbodylen = max((len(a[1]) for a in self.article_list))
+        print('Max length of body:', self.maxbodylen)
+        self.maxbodylen = max((self.maxbodylen, body_trunc))
+        print('Truncated body length:', self.maxbodylen)
         self.stancelen = len(self.int2stancemap)
-        print('Max length of title: ', self.maxtitlelen)
-        print('Max length of body: ', self.maxbodylen)
-        print('Kinds of stance: ', self.stancelen)
-        print('Stances: ', self.stance2intmap)
+        print('Kinds of stance:', self.stancelen)
+        print('Stances:', self.stance2intmap)
 
-        print('Loading GoogleNews-vectors-negative300.bin...')
+        print('Loading %s' % word2vecmodelfile)
         self._model = KeyedVectors.load_word2vec_format(word2vec_bin_path, binary=True)
         self.vecdim = self._modeldim = self._model.vector_size
-        print('Dim of word vector: ', self.vecdim)
+        print('Dim of word vector:', self.vecdim)
 
-        self.dataiter = self.data()
+        self.trainsize = round(train_ratio * len(self.article_list))
+        self.validatesize = len(self.article_list) - self.trainsize
+        trainsize = self.trainsize
+        self.train_article_list = self.article_list[:trainsize]
+        self.validate_article_list = self.article_list[trainsize:]
 
-    def data(self):
+        self.trainvecs = list(self._data(self.train_article_list))
+        self.validatevecs = list(self._data(self.validate_article_list))
+
+        self.train_dataiter = iter(self.trainvecs)
+        self.validate_dataiter = iter(self.validatevecs)
+
+    def train_data(self):
+        return iter(self.trainvecs)
+
+    def validate_data(self):
+        return iter(self.validatevecs)
+
+    def train_get_batch(self, batch_size):
+        binput = []
+        boutput = []
+        for tx in range(batch_size):
+            try:
+                tdata = next(self.train_dataiter)
+            except StopIteration:
+                self.train_dataiter = self.train_data()
+                break
+            binput.append(tdata[0].astype(np.float64))
+            boutput.append(tdata[1])
+        inputshape = binput[0].shape
+        binput = torch.FloatTensor(binput).view(-1, 1, inputshape[0], inputshape[1])
+        boutput = torch.LongTensor(boutput).view(-1)
+        return binput, boutput
+
+    def validate_get_batch(self, batch_size):
+        binput = []
+        boutput = []
+        for tx in range(batch_size):
+            try:
+                tdata = next(self.validate_dataiter)
+            except StopIteration:
+                self.validate_dataiter = self.validate_data()
+                break
+            binput.append(tdata[0].astype(np.float64))
+            boutput.append(tdata[1])
+        inputshape = binput[0].shape
+        binput = torch.FloatTensor(binput).view(-1, 1, inputshape[0], inputshape[1])
+        boutput = torch.LongTensor(boutput).view(-1)
+        return binput, boutput
+
+    def _data(self, alist):
         '''
         Get the iterator of all vectorized articles.
         :return: a new iterator of vectorized articles 
         '''
         return map(lambda article: (
-            self._get_vec_from_words(article[0], fillzerosto=self.maxtitlelen),
-            self._get_vec_from_words(article[1], fillzerosto=self.maxbodylen), self.stance2intmap[article[2]]),
-                   self.article_list)
+            self._get_vec_from_words(article[0], self.maxtitlelen, article[1], self.maxbodylen),
+            self.stance2intmap[article[2]]),
+                   alist)
 
     def get_article_list(self):
         return self.article_list
-
-    def get_batch(self, batch_size):
-        binput = []
-        boutput = []
-        for tx in range(batch_size):
-            try:
-                tdata = next(self.dataiter)
-            except StopIteration:
-                self.dataiter = self.data()
-                # tdata = next(self.dataiter)
-                break
-            binput.append(np.vstack((tdata[0], tdata[1])).astype(np.float64))
-            boutput.append(tdata[2])
-        inputshape = binput[0].shape
-        binput = Variable(torch.FloatTensor(binput).view(-1, 1, inputshape[0], inputshape[1]), requires_grad=True)
-        boutput = Variable(torch.LongTensor(boutput).view(-1), requires_grad=False)
-        return binput, boutput
 
     def _clean(self, s):
         return map(lambda x: x.lower(), re.findall(r'[a-zA-Z]+', s, flags=re.UNICODE))
@@ -92,37 +129,29 @@ class Data(object):
     def _remove_stopwords(self, l):
         return [w for w in l if w not in feature_extraction.text.ENGLISH_STOP_WORDS and len(w) > 1]
 
-    def _preprocess_text(self, s):
-        return self._remove_stopwords(self._clean(s))
+    def _preprocess_text(self, s, trunc):
+        if self.remove_stopwords:
+            res = self._remove_stopwords(self._clean(s))
+        else:
+            res = list(self._clean(s))
+        return res[:trunc]
 
-    def _get_vec_from_words(self, s, fillzerosto=None):
-        '''
-        Transform string s to numpy 2d array. Fill zeros if size is smaller.
-        :param s: string
-        :param fillzerosto: if given, fill zeros to satisfy the given length
-        :return: numpy 2d array
-        '''
-        vecs = None
-        for word in s:
-            vec = None
+    def _get_vec_from_words(self, s0, fillzerosto0, s1, fillzerosto1):
+        lens0 = len(s0)
+        lens1 = len(s1)
+        vecs = [np.zeros(self._modeldim) for t in range(fillzerosto0 + fillzerosto1)]
+        for t in range(lens0):
             try:
-                vec = self._model.word_vec(word)
+                vecs[t] = self._model.word_vec(s0[t])
             except KeyError:
-                vec = np.zeros(self._modeldim)
-            if vecs is None:
-                vecs = vec
-            else:
-                vecs = np.vstack((vecs, vec))
-        if fillzerosto:
-            tofill = fillzerosto - vecs.shape[0]
-            for t in range(tofill):
-                vecs = np.vstack((vecs, np.zeros(vecs.shape[1])))
-        return vecs
+                pass
+        for t in range(lens1):
+            try:
+                vecs[t + fillzerosto0] = self._model.word_vec(s1[t])
+            except KeyError:
+                pass
+        return np.vstack(tuple(vecs))
 
 
 if __name__ == '__main__':
-    tdata = Data()
-    for t in range(3):
-        traindata = tdata.data()
-        for t2 in range(1):
-            print(next(traindata))
+    pass
